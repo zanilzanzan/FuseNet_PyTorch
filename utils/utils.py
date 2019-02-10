35b8260a6431304f7ad521ce6ba7da58f2082b9e
zanilzanzan <anilsonmez@hotmail.com.tr>
2019-02-10 18:03:56 +0100

    Take class distribution weights from text

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,24 @@
+import torch
+
+
+def get_class_weights(dset_name='sun'):
+    with open('./weights.txt', 'r') as f:
+        lines = f.read().splitlines()
+    dset_weights, read = [], False
+
+    for line in lines:
+        if line.lower().find(dset_name) is not -1 and not read:
+            read = True
+            continue
+        if line == '-':
+            read = False
+        if read:
+            weights = line.split(', ')
+            for weight in weights:
+                dset_weights.append(float(weight))
+    return torch.cuda.FloatTensor(dset_weights)
+
+
 def print_time_info(start_time, end_time):
     print('[INFO] Start and end time of the last session: %s - %s'
           % (start_time.strftime('%d.%m.%Y %H:%M:%S'), end_time.strftime('%d.%m.%Y %H:%M:%S')))
